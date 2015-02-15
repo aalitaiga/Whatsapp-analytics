f0c7e02618952fd8ccddf0f5d08a80065ed5956d
aalitaiga <adrien.alitaiga@gmail.com>
2015-02-15 02:40:33 +0100

    New regex, dataframe creation and cleaning

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 # -*- coding: UTF-8 -*-
 
 """ Script to analyse whatsapp data """
-from datetime import datetime as dt
+import datetime as dt
 import pandas as pd
+import re
 
+# Shorten the regex
+string = r"""(?P<date>\d{1,2} \w{3,4} à \d{1,2}:\d{2}) - (?P<name>[\+\w]+(?::\s*[\w\+]+)*|[\+\w\s]+?)(?:\s(?P<action>a été ajouté\(e\)|est parti|a retiré [^\n]*|a changé (?:le sujet en ".*?"|l'icône de ce groupe|de [^à]+à [^\n]*))|:)(?P<message>.*?)(?=\s*\d{1,2} \w{3,4} à \d{1,2}:\d{2}|$)"""
+regex = re.compile(string)
 
-def get_data(filename):
-	with open(filename, 'r') as f:
-		lines = f.readlines()
-
-	d = []
-	for line  in lines:
-		d.append(process_line)
-
-	df = pd.DataFrame(d)
-	return df
-
-
-def process_line(line):
-	d = {
+french_d = {
 		'Jan': 'January', 'Feb': 'February', 'Mars': 'March', 'Avr': 'April',
 		'Mai': 'May', 'Juin': 'June', 'Juil': 'July', 'Sep': 'September',
 		'Oct': 'October', 'Nov': 'November', 'Dec': 'December'
 	}
 
-	date, rest_line = line.split(" - ")
-	date = date.split(" ")
-	date[2] = d[date[2]]
-	new_date = " ".join(date)
-	date = dt.datetime(new_date, "%d %B à %X")
-	author, text = rest_line.split(": ")
-
-	return {
-		'author': author, 'date': date, 'text': text
-	}
+def get_data(filename):
+	with open(filename, 'r') as f:
+		lines = f.readlines()
+	data = ''.join(lines)
+	matches = re.finditer(regex, data)
+	return pd.DataFrame([match.groupdict() for match in matches])
 
 
+def clean_data(df):
+	df.apply(format_date, axis=1)
+	# Find a way to also get the year
+	df['date'] = pd.to_datetime(df['date'], format="%d %B %H:%M")
+	return df
 
+	
+def format_date(row):
+	day, month, _, message_time = row['date'].split(' ')
+	#import ipdb; ipdb.set_trace()
+	# zero-padding for day and time
+	if len(day) == 1:
+		day = '0'.append(day)
+	if len(message_time.split(':')[0]) == 1:
+		message_time = '0'.append(message_time)
+	row['date'] = ' '.join([day, french_d[month], message_time])
+	return row
+
+
+# English regex
+# (?P<date>\d{2}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}): 
+# (?P<name>\w+(?::\s*\w+)*|[\w\s]+?)(?:\s+(?P<action>joined|left|was removed|changed 
+#	the (?:subject to “\w+”|group icon))|:\s(?P<message>(?:.+|\n(?!\n))+))
+#
+# French regex
+# (?P<date>\d{1,2} \w{3,4} à \d{1,2}:\d{2}) - 
+# (?P<name>[\+\w]+(?::\s*[\w\+]+)*|[\+\w\s]+?)(?:\s(?P<action>a été ajouté\(e\)
+# |est parti|a retiré [^\n]*|a changé (?:le sujet en ".*?"|l'icône de ce groupe
+# de [^à]+à [^\n]*))|:)(?P<message>.*?)(?=\s*\d{1,2} \w{3,4} à \d{1,2}:\d{2}|$)
+
+if __name__ == '__main__':
+	print clean_data(get_data('1H_2.txt'))
 
--- test.py
+++ test.py
@@ -1,21 +1,28 @@
 # -*- coding: UTF-8 -*-
 
 import unittest
+import datetime as dt
 from main import (
 	get_data, process_line
 )
-from datetime import datetime as dt
-
 
 
 class WhatsappAnalyticsTest(unittest.TestCase):
 
 	def test_process_line(self):
-		line = "7 Nov à 14:10 - Samy Ouardini: Mehdi lova lova"
+		line = "07 Nov à 14:10 - Samy Ouardini: Mehdi lova lova"
 		processed_line = process_line(line)
 
 		self.assertTrue(isinstance(processed_line, dict))
 		self.assertEqual(processed_line['author'], 'Samy Ouardini')
-		self.assertEqual(processed_line['text'], "Medhi lova lova")
-		print processed_line['date']
+		self.assertEqual(processed_line['text'], "Mehdi lova lova")
 		self.assertTrue(isinstance(processed_line['date'], dt.datetime))
+
+	def test_get_data(self):
+		data = get_data("1H.txt")
+		self.assertFalse(data.empty)
+		print data.head()
+
+
+if __name__ == '__main__':
+	unittest.main()
